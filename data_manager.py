922bef65a3e108947c5cc7a319fefd04556adee5
Bruno Bertoni <brunobertoni68@gmail.com>
2026-03-26 13:48:58 -0300

    cambie la ubicacion de los arboles y agregue mas arboles

--- data_manager.py
+++ data_manager.py
@@ -1,4 +1,4 @@
-# Bruno Bertoni
+#Bruno1
 
 inventario = []
 
@@ -6,15 +6,15 @@ def inicializar_datos():
     global inventario
 
     inventario = [
-        {"especie": "Jacarandá", "estado": "Saludable", "ubicacion": "Plaza central"},
-        {"especie": "Roble", "estado": "Enfermo", "ubicacion": "Av. San Martín"},
-        {"especie": "Pino", "estado": "Saludable", "ubicacion": "Parque industrial"},
-        {"especie": "Ceibo", "estado": "Saludable", "ubicacion": "Costanera"},
+        {"especie": "Jacarandá", "estado": "Saludable", "ubicacion": "Plaza San Martin"},
+        {"especie": "Roble", "estado": "Enfermo", "ubicacion": "Plaza Mitre"},
+        {"especie": "Pino", "estado": "Saludable", "ubicacion": "Parque de la Familia"},
+        {"especie": "Ceibo", "estado": "Saludable", "ubicacion": "Plaza Bicentenario"},
         {"especie": "Lapacho", "estado": "Enfermo", "ubicacion": "Barrio Norte"},
         {"especie": "Eucalipto", "estado": "Saludable", "ubicacion": "Ruta 3"},
-        {"especie": "Sauce", "estado": "Saludable", "ubicacion": "Río local"},
-        {"especie": "Álamo", "estado": "Enfermo", "ubicacion": "Parque Sarmiento"},
-        {"especie": "Fresno", "estado": "Saludable", "ubicacion": "Escuela primaria"},
+        {"especie": "Sauce", "estado": "Saludable", "ubicacion": "Paseo del Caminante"},
+        {"especie": "Álamo", "estado": "Enfermo", "ubicacion": "Biblioteca Sarmiento"},
+        {"especie": "Fresno", "estado": "Saludable", "ubicacion": "Escuela PRoA"},
         {"especie": "Palmera", "estado": "Saludable", "ubicacion": "Entrada de la ciudad"}
     ]
 
