0ee0bf07f3119eced3f7f853ad4ceab6c185d7dc
Bruno Bertoni <brunobertoni68@gmail.com>
2026-03-26 13:12:11 -0300

    Tarea 1: inicialización de datos

---
+++ data_manager.py
@@ -0,0 +1,24 @@
+# Bruno (o tu nombre)
+
+inventario = []
+
+def inicializar_datos():
+    global inventario
+
+    inventario = [
+        {
+            "especie": "Jacarandá",
+            "estado": "Saludable",
+            "ubicacion": "Plaza central"
+        },
+        {
+            "especie": "Roble",
+            "estado": "Enfermo",
+            "ubicacion": "Av. San Martín"
+        },
+        {
+            "especie": "Pino",
+            "estado": "Saludable",
+            "ubicacion": "Parque industrial"
+        }
+    ]
